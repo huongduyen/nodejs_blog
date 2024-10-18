2e873253c2bc894171e0d29131b8720a4d5739c7
duyen_van <duyen_van@datahouse.com>
2024-10-18 08:43:59 +0700

    Initial commit

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/tin-tuc', (req, res) => {
+  var a = 1;
+  var b =2;
+
+  var c = a+ b;
+  return res.send('Hello World!')
 })
 
 app.listen(port, () => {

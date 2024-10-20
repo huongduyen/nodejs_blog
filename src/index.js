4db6e413cf3d8c3b2386a836a2a29106ba336633
huongduyen <huongduyenvan3012@gmail.com>
2024-10-20 08:50:42 +0700

    Add template engine

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Template engine: handlebars
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources\\views'));
+
+console.log("PATH", path.join(__dirname,'resources\\views'));
+
+app.get('/', (req, res) => {
+  return res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  return res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
